1c9f34c707b08c4725ca9dda559e1d06801a22f1
baldsealion <93551277+baldsealion@users.noreply.github.com>
2023-02-21 21:19:54 -0800

    Add files via upload

---
+++ toc.js
@@ -0,0 +1,17 @@
+<html>
+<body>
+    <script>
+      function showChild(event) 
+      {
+        var child = e.target.nextElementSibling;
+        if (child.style.display == "none") 
+        {
+          child.style.display = "block";}
+        else 
+        {
+          child.style.display = "none";}
+      }
+      
+    </script>
+</body>
+</html>
